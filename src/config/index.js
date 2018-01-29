20d55f88cce9872b21e44f5af27d251d2cff1aef
mango <feifeidemao>
2018-01-29 22:35:03 +0800

    测试

--- src/config/index.js
+++ src/config/index.js
@@ -43,36 +43,37 @@ module.exports = {
         var _this = this;
         return new Promise((resolve, reject) => {
 
+            _this.Compare().then()
 
         });
 
 
-        var promise = new Promise(function(resolve, reject) {
-            var conFile = JSON.parse(fs.readFileSync(configJsonFile));
-            auth.auth(conFile.url, conFile.data)
-                .then((value) => {
-                    // console.log("成功",value);
-                    var time = new Date().getTime(); //获得请求成功时的时间的毫秒值
-                    time = time + 3600000; //time 是过期时间
-                    conFile.expTime = time; //给config文件添加过期时间字段
-                    conFile.accessToken = value.data.accessToken; //将accessToken存到config文件
-                    fs.writeFile(configJsonFile, JSON.stringify(conFile), function(error) {
-                        if (error) {
-                            // console.log("写入失败",error);
-                            reject("error,写入失败");
-                            return;
-                        }
-                        // console.log("写入成功");
-                        resolve("success");
+        // var promise = new Promise(function(resolve, reject) {
+        //     var conFile = JSON.parse(fs.readFileSync(configJsonFile));
+        //     auth.auth(conFile.url, conFile.data)
+        //         .then((value) => {
+        //             // console.log("成功",value);
+        //             var time = new Date().getTime(); //获得请求成功时的时间的毫秒值
+        //             time = time + 3600000; //time 是过期时间
+        //             conFile.expTime = time; //给config文件添加过期时间字段
+        //             conFile.accessToken = value.data.accessToken; //将accessToken存到config文件
+        //             fs.writeFile(configJsonFile, JSON.stringify(conFile), function(error) {
+        //                 if (error) {
+        //                     // console.log("写入失败",error);
+        //                     reject("error,写入失败");
+        //                     return;
+        //                 }
+        //                 // console.log("写入成功");
+        //                 resolve("success");
 
-                    });
-                })
-                .catch((error) => {
-                    // console.log("失败",error);
-                    reject("error,失败");
-                });
-        });
-        return promise;
+        //             });
+        //         })
+        //         .catch((error) => {
+        //             // console.log("失败",error);
+        //             reject("error,失败");
+        //         });
+        // });
+        // return promise;
     }
 };
 
